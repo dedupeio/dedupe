3dc94090ad79b680d4a5c5dc2fc80c456fec6fc4
Derek Eder <derek.eder@gmail.com>
2012-04-20 12:33:25 -0500

    implemted brute force find duplicates function

--- dedupe.py
+++ dedupe.py
@@ -13,5 +13,47 @@ def canonicalImport(filename) :
             data_d[i] = instance
 
     return(data_d, header)
-        
 
+def dataModel() :
+  return  {'fields': 
+            {'name' : {'type': 'String', 'weight' : 1}, 
+             'address' : {'type' :'String', 'weight' : 1},
+             'city' : {'type': 'String', 'weight' : 1},
+             'cuisine' : {'type': 'String', 'weight' : 1}
+            },
+           'bias' : 0}
+
+def identifyCandidates(data_d) :
+  return [data_d.keys()]
+
+def findDuplicates(candidates, data_d, data_model, threshold) :
+  import distance #libdistance library http://monkey.org/~jose/software/libdistance/
+  import itertools
+  duplicateScores = []
+
+  for candidates_set in candidates :
+    for pair in itertools.combinations(candidates_set, 2):
+      scorePair = {}
+      score = data_model['bias'] 
+      fields = data_model['fields']
+      for name in fields :
+        if fields[name]['type'] == 'String' :
+          distanceFunc = distance.levenshtein
+        x = distanceFunc(data_d[pair[0]][name],data_d[pair[1]][name])
+        score += x * fields[name]['weight']
+      scorePair[pair] = score
+      #print (pair, score)
+      if score < threshold :
+        print (data_d[pair[0]],data_d[pair[1]])
+        print score
+        duplicateScores.append(scorePair)
+  
+  return duplicateScores
+
+if __name__ == '__main__':
+  data_d, header = canonicalImport("./datasets/restaurant-nophone.csv")
+  data_model = dataModel()
+  candidates = identifyCandidates(data_d)
+  print "finding duplicates"
+  dupes = findDuplicates(candidates, data_d, data_model, 5)
+  #print dupes
