701324123853aa4ef4aed10bea474b1e4fb07e90
Derek Eder <derek.eder@gmail.com>
2012-04-27 12:11:32 -0500

    creating training data based on known duplicates and sampling non-duplicates

--- dedupe.py
+++ dedupe.py
@@ -1,10 +1,11 @@
 import itertools
+import distance #libdistance library http://monkey.org/~jose/software/libdistance/
 
 def canonicalImport(filename) :
     import csv
 
     data_d = {}
-    training_d = {}
+    duplicates_d = {}
     with open(filename) as f :
         reader = csv.reader(f)
         header = reader.next()
@@ -13,19 +14,19 @@ def canonicalImport(filename) :
             instance = {}
             for j, col in enumerate(row) :
               if header[j] == 'unique_id' :
-                training_d.setdefault(col, []).append(i)
+                duplicates_d.setdefault(col, []).append(i)
               else :
                 instance[header[j]] = col.strip()
                 
             data_d[i] = instance
 
-    training_l = []
-    for unique_id in training_d :
-      if len(training_d[unique_id]) > 1 :
-        for pair in itertools.combinations(training_d[unique_id], 2) :
-          training_l.append(pair)
+    duplicates_l = []
+    for unique_id in duplicates_d :
+      if len(duplicates_d[unique_id]) > 1 :
+        for pair in itertools.combinations(duplicates_d[unique_id], 2) :
+          duplicates_l.append(pair)
 
-    return(data_d, header, training_l)
+    return(data_d, header, duplicates_l)
 
 def dataModel() :
   return  {'fields': 
@@ -40,7 +41,6 @@ def identifyCandidates(data_d) :
   return [data_d.keys()]
 
 def findDuplicates(candidates, data_d, data_model, threshold) :
-  import distance #libdistance library http://monkey.org/~jose/software/libdistance/
   duplicateScores = []
 
   for candidates_set in candidates :
@@ -48,11 +48,10 @@ def findDuplicates(candidates, data_d, data_model, threshold) :
       scorePair = {}
       score = data_model['bias'] 
       fields = data_model['fields']
+
+      distances = calculateDistance(data_d[pair[0]], data_d[pair[1]], fields)
       for name in fields :
-        if fields[name]['type'] == 'String' :
-          distanceFunc = distance.levenshtein
-        x = distanceFunc(data_d[pair[0]][name],data_d[pair[1]][name])
-        score += x * fields[name]['weight']
+        score += distances[name] * fields[name]['weight']
       scorePair[pair] = score
       #print (pair, score)
       if score < threshold :
@@ -62,14 +61,54 @@ def findDuplicates(candidates, data_d, data_model, threshold) :
   
   return duplicateScores
 
+def calculateDistance(instance_1, instance_2, fields) :
+  distances_d = {}
+  for name in fields :
+    if fields[name]['type'] == 'String' :
+      distanceFunc = distance.levenshtein
+    x = distanceFunc(instance_1[name],instance_2[name])
+    distances_d[name] = x
+
+  return distances_d
+
+def createTrainingPairs(data_d, duplicates_l, n) :
+  import random
+  train_pairs = {}
+  for pair in duplicates_l :
+    train_pairs[pair] = 1
+  while len(train_pairs) < n :
+    random_pair = tuple(random.sample(data_d.keys(), 2))
+    if random_pair not in train_pairs : 
+      train_pairs[random_pair] = 0
+      
+  return(train_pairs)
+
+def createTrainingData(data_d, duplicates_l, n, data_model) :
+  train_pairs = createTrainingPairs(data_d, duplicates_l, n)
+
+  training_data = []
+  for pair in train_pairs :
+    distances = calculateDistance(data_d[pair[0]], data_d[pair[1]], data_model['fields'])
+    training_data.append((distances, train_pairs[pair]))
+
+  return training_data
+
 if __name__ == '__main__':
-  data_d, header, training_l = canonicalImport("./datasets/restaurant-nophone-training.csv")
+  data_d, header, duplicates_l = canonicalImport("./datasets/restaurant-nophone-training.csv")
   data_model = dataModel()
   candidates = identifyCandidates(data_d)
   #print "training data: "
-  #print training_l
-  print "number of duplicates from training data "
-  print len(training_l)
+  #print duplicates_l
+  
+  print "number of known duplicates: "
+  print len(duplicates_l)
+
+  train_pairs = createTrainingData(data_d, duplicates_l, 500, data_model)
+  print "training data from known duplicates: "
+  print train_pairs
+  print "number of training items: "
+  print len(train_pairs)
+
   print "finding duplicates ..."
   dupes = findDuplicates(candidates, data_d, data_model, 5)
-  print dupes
+  #print dupes
