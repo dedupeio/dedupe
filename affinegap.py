28a9e026efb8a99635d2333666a0b7226c2639ac
Forest Gregg <fgregg@uchicago.edu>
2012-05-07 00:29:53 -0500

    speed, bug, and normalization

--- affinegap.py
+++ affinegap.py
@@ -7,61 +7,80 @@ def print_matrix(m):
             spTupel = spTupel + (column, )
         print "%3i"*breite % spTupel
 
-#string1 = raw_input('first word: ')
-#string2 = raw_input('second word: ')
+#calculate the affine gap distance between 2 strings default weights
+#taken from page 28 of Bilenko's Ph. D dissertation: Learnable
+#Similarity Functions and their Application to Record Linkage and
+#Clustering
 
-def defineCharacterScore(matchWeight, mismatchWeight) :
-  def characterScore (char1, char2) :
-    if (char1 == char2) :
-      return matchWeight
-    else :
-      return mismatchWeight
-  return characterScore
-  
-#calculate the affine gap distance between 2 strings
-#default weights taken from page 28 of Bilenko's Ph. D dissertation: Learnable Similarity Functions and their Application to Record Linkage and Clustering
-def affineGapDistance(string1, string2, matchWeight = -5, mismatchWeight = 5, gapWeight = 5, spaceWeight = 1):
+def affineGapDistance(string1, string2,
+                      matchWeight = -5,
+                      mismatchWeight = 5,
+                      gapWeight = 5,
+                      spaceWeight = 1):
   
   length1 = len(string1)
   length2 = len(string2)
   
-  #set up scoring function with match and mismatch weights
-  score = defineCharacterScore(matchWeight, mismatchWeight)
-  
-  #v_matrix = distance
-  #e_matrix = distance when string1 prefix is left aligned to string2 
-  #f_matrix = distance when string1 prefix is right aligned to string2
-  #g_matrix = distence when string1 prefix and string2 prefix are aligned
 
-  #define base case of recurrences
-  v_matrix = [[(i * spaceWeight + gapWeight) for i in range(length1 + 1)]] * (length2 + 1)
-  for row in range(length2 + 1):
-    v_matrix[row] = [(i * spaceWeight + gapWeight) for i in range(row, row + length1 + 1)]
-  
   #set up recurrence matrices
-  e_matrix = [x[:] for x in v_matrix]
-  f_matrix = [x[:] for x in v_matrix]
-  g_matrix = [x[:] for x in v_matrix]
-  
-  for row in range(1,length2 + 1) :
-    for col in range(1,length1 + 1) :
-      g_matrix[row][col] = v_matrix[row-1][col-1] + score(string1[col-1],string2[row-1])
+
+  #v_matrix = minimum distance matrix
+  v_matrix = [[None for _ in xrange(length1+1)] for _ in xrange(length2+1)]
+
+  # define base case of recurrences
+  # Matrix(0,0) = 0
+  # Matrix(0,j) = gapWeight + spaceWeight * j
+  v_matrix[0] = f_matrix = [(j * spaceWeight + gapWeight) for j in xrange(length1 + 1)]
+  v_matrix[0][0] = 0
+
+  #e_matrix: minimum distance matrix when string1 prefix is left aligned to
+  #string2
+  #e_matrix = [row[:] for row in v_matrix]
+
+  #f_matrix: minimum distance matrix when string1 prefix is right
+  #aligned to string2
+  #f_matrix = [row[:] for row in v_matrix]
+
+  for i in xrange(1,length2 + 1) :
+    # Matrix(i,0) = gapWeight + spaceWeight * i
+    v_matrix[i][0] = e = i * spaceWeight + gapWeight
+
+    for j in xrange(1,length1 + 1) :
+      if string2[i-1] == string1[j-1] :  
+        g = v_matrix[i-1][j-1] + matchWeight
+      else :
+        g = v_matrix[i-1][j-1] + mismatchWeight
+
+      e = min(e, v_matrix[i][j-1] + gapWeight) + spaceWeight
+
       #print "g: ", g_matrix[row][col]
-      e_matrix[row][col] = min(e_matrix[row][col-1], v_matrix[row][col-1] + gapWeight) + spaceWeight
+      #e_matrix[i][j] = (min(e_matrix[i][j-1],
+      #                      v_matrix[i][j-1] + gapWeight)
+      #                  + spaceWeight)
       #print "e: ", e_matrix[row][col]
-      f_matrix[row][col] = min(f_matrix[row-1][col], v_matrix[row-1][col] + gapWeight) + spaceWeight
+      f_matrix[j] = (min(f_matrix[j],
+                         v_matrix[i-1][j] + gapWeight)
+                        + spaceWeight)
       #print "f: ", f_matrix[row][col]
-      v_matrix[row][col] = min(e_matrix[row][col], f_matrix[row][col], g_matrix[row][col])
+      v_matrix[i][j] = min(e,
+                           f_matrix[j],
+                           g)
       #print "v: ", v_matrix[row][col]
   
   #print "Affine Gap v_matrix:"
-  
   #print_matrix(g_matrix)
   #print_matrix(e_matrix)
   #print_matrix(f_matrix)
   #print_matrix(v_matrix)
   
-  return v_matrix[length2][length1]
-    
-#distance = affineGapDistance(string1, string2)		
-#print 'The Affine Gap Distance of ',string1, ' and ', string2, ' is ', distance
+  return v_matrix[length2][length1]/float(length1 + length2)
+
+if __name__ == "__main__" :
+    import cProfile
+    def test() :
+        for i in xrange(100000) :
+            string1 = 'asdf'
+            string2 = 'fdsa'
+            distance = affineGapDistance(string1, string2)		
+
+    cProfile.run('test()')
