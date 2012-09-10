ba8047c95a8545caa97d83a71490328ef35274ff
Forest Gregg <fgregg@uchicago.edu>
2012-09-10 10:01:27 -0500

    declare dependencies in setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
   include_dirs = [numpy.get_include()],
   ext_modules = [Extension("dedupe.affinegap", ["src/affinegap.c"]),
                  Extension("dedupe.lr", ["src/lr.c"]),],
+  install_requires = ['numpy', 'fastcluster', 'hcluster']
 )
