06da6a454aa468dd71e101573852bd87699e9e06
Forest Gregg <fgregg@uchicago.edu>
2012-09-10 13:39:50 -0500

    docstrings for predicates

--- dedupe/predicates.py
+++ dedupe/predicates.py
@@ -3,40 +3,41 @@
 import re
 
 
-# returns the field as a tuple
-
 def wholeFieldPredicate(field):
+    """return the whole field"""
     return (field, )
 
 
-# returns the tokens in the field as a tuple, split on whitespace
-
 def tokenFieldPredicate(field):
+    """returns the tokens"""
     return tuple(field.split())
 
 
-# Contain common integer
-
 def commonIntegerPredicate(field):
+    """"return any integers"""
     return tuple(re.findall("\d+", field))
 
 
 def nearIntegersPredicate(field):
+    """return any integers N, N+1, and N-1"""
     ints = sorted([int(i) for i in re.findall("\d+", field)])
     return tuple([(i - 1, i, i + 1) for i in ints])
 
 
 def commonFourGram(field):
+    """return 4-grams"""
     return tuple([field[pos:pos + 4] for pos in xrange(0, len(field),
                  4)])
 
 
 def commonSixGram(field):
+    """"return 6-grams"""
     return tuple([field[pos:pos + 6] for pos in xrange(0, len(field),
                  6)])
 
 
 def sameThreeCharStartPredicate(field):
+    """return first three characters"""
     if len(field) < 3:
         return ()
 
@@ -44,6 +45,7 @@ def sameThreeCharStartPredicate(field):
 
 
 def sameFiveCharStartPredicate(field):
+    """return first five characters"""
     if len(field) < 5:
         return ()
 
@@ -51,9 +53,8 @@ def sameFiveCharStartPredicate(field):
 
 
 def sameSevenCharStartPredicate(field):
+    """return first seven charactesr"""
     if len(field) < 7:
         return ()
 
     return (field[:7], )
-
-
