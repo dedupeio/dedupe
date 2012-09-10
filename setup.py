83a4967984a97cb16bea256f3d02eb5104b5233d
Forest Gregg <fgregg@uchicago.edu>
2012-09-10 10:03:11 -0500

    pep8

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from distutils.core import setup, Extension
 import numpy
 
 setup(
-  name='Dedupe',
-  version='0.3',
-  packages=['dedupe', 'dedupe.clustering',],
-  include_dirs = [numpy.get_include()],
-  ext_modules = [Extension("dedupe.affinegap", ["src/affinegap.c"]),
-                 Extension("dedupe.lr", ["src/lr.c"]),],
-  install_requires = ['numpy', 'fastcluster', 'hcluster']
-)
+    name='Dedupe',
+    version='0.3',
+    packages=['dedupe', 'dedupe.clustering'],
+    include_dirs=[numpy.get_include()],
+    ext_modules=[Extension('dedupe.affinegap', ['src/affinegap.c']),
+                 Extension('dedupe.lr', ['src/lr.c'])],
+    install_requires=['numpy', 'fastcluster', 'hcluster'],
+    )
