e297c9f0edc60ed10ad87f2366ec9acdfc127d1e
Forest Gregg <fgregg@uchicago.edu>
2013-02-09 12:41:13 -0600

    added some info to setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ import numpy
 
 setup(
     name='Dedupe',
+    url='https://github.com/open-city/dedupe',
     version='0.3',
     packages=['dedupe', 'dedupe.clustering'],
     ext_modules=[Extension('dedupe.affinegap', ['src/affinegap.c']),
                  Extension('dedupe.lr',
                            sources=['src/lr.c'],
                            include_dirs=[numpy.get_include()])],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
     install_requires=['numpy', 'fastcluster', 'hcluster', 'networkx'],
+    long_description=open('README.md').read(),
     )
