8ec793d7c1a9493c67702b55505193b9d9b528d1
Forest Gregg <fgregg@uchicago.edu>
2013-02-10 22:16:56 -0600

    try to make install not require numpy installed separately

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from distutils.core import setup, Extension
-import numpy
+
+# from Michael Hoffman's http://www.ebi.ac.uk/~hoffman/software/sunflower/
+class NumpyExtension(Extension):
+    def __init__(self, *args, **kwargs):
+        Extension.__init__(self, *args, **kwargs)
+  
+        self._include_dirs = self.include_dirs
+        del self.include_dirs # restore overwritten property
+  
+    # warning: Extension is a classic class so it's not really read-only
+    @property
+    def include_dirs(self):
+        from numpy import get_include
+  
+        return self._include_dirs + [get_include()]
 
 setup(
     name='Dedupe',
     version='0.3',
     packages=['dedupe', 'dedupe.clustering'],
+
     ext_modules=[Extension('dedupe.affinegap', ['src/affinegap.c']),
-                 Extension('dedupe.lr',
-                           sources=['src/lr.c'],
-                           include_dirs=[numpy.get_include()])],
+                 NumpyExtension('dedupe.lr',
+                           sources=['src/lr.c'])],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
     install_requires=['numpy', 'fastcluster', 'hcluster', 'networkx'],
+
+    long_description=open('README.md').read(),
     )
