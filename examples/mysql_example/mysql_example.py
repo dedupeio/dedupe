a92a3902a7b3646a21652c0ded0824cb1ca80bd6
Derek Eder <derek.eder@gmail.com>
2013-02-25 22:20:16 -0600

    another pass on mysql_example documentation

--- examples/mysql_example/mysql_example.py
+++ examples/mysql_example/mysql_example.py
@@ -5,9 +5,9 @@ This is an example of working with very large data. There are about
 700,000 unduplicated donors in this database of Illinois political
 campaign contributions.
 
-While can these donor records in memory, we cannot store all the
-comparisons we will plan to make. We will have to read the pairs on
-demand, from disk
+With such a large set of input data, we cannot store all the comparisons 
+we need to make in memory. Instead, we will read the pairs on demand
+from the MySQL database.
 """
 import os
 import itertools
@@ -19,6 +19,10 @@ import MySQLdb
 import MySQLdb.cursors
 
 import dedupe
+
+# ### Logging
+
+# Dedupe uses Python logging to show or suppress verbose output. Added for convenience.
 
 optp = optparse.OptionParser()
 optp.add_option('-v', '--verbose', dest='verbose', action='count',
@@ -34,8 +38,9 @@ logging.basicConfig(level=log_level)
 
 # ### Setup
 
-# When we compare records, we don't care about differences in case.
-# Lowering the case in SQL is much faster than in Python.
+# Create a select function that pulls in our campaign donor info.
+# When we compare records, we don't care about differences in casing,
+# so we lower the case (doing this in SQL is much faster than in Python).
 DONOR_SELECT = "SELECT donor_id, LOWER(city) AS city, " \
                "LOWER(first_name) AS first_name, " \
                "LOWER(last_name) AS last_name, " \
@@ -45,18 +50,15 @@ DONOR_SELECT = "SELECT donor_id, LOWER(city) AS city, " \
 
 
 def getSample(c, sample_size, id_column, table):
-  """
-  Returns a random sample of pairs of donors of a given size from a
-  MySQL table.  Depending on your database engine, you will need to
-  come up with a similar function.
-
-  The id column must contain unique, sequential itegers starting at 0
-  or 1
-  """
+  '''
+  Returns a random sample of a given size of records pairs from a given
+  MySQL table.
+  '''
 
   c.execute("SELECT MAX(%s) FROM %s" , (id_column, table))
   num_records = c.fetchone().values()[0]
 
+  # dedupe expects the id column to contain unique, sequential itegers starting at 0 or 1
   random_pairs = dedupe.randomPairs(num_records, sample_size, zero_indexed=False)
 
   temp_d = {}
@@ -72,13 +74,16 @@ def getSample(c, sample_size, id_column, table):
   
   return tuple(record_pairs for pair in random_pair_generator())
 
+# Switch to our working directory and set up our settings and training
+# file locations
 os.chdir('./examples/mysql_example/')
 settings_file = 'mysql_example_settings.json'
 training_file = 'mysql_example_training.json'
 
-
-
+# keep track of our execution time
 start_time = time.time()
+
+# ### Create our database connection
 
 # You'll need to copy `examples/mysql_example/mysql.cnf_LOCAL` to
 # `examples/mysql_example/mysql.cnf` and fill in your mysql
@@ -89,16 +94,16 @@ con = MySQLdb.connect(db='contributions',
 
 c = con.cursor()
 
-# ### Learn or read how to compare records and good blocking rules
-
+# ### Data model setup and training
 
 if os.path.exists(settings_file):
     print 'reading from ', settings_file
     deduper = dedupe.Dedupe(settings_file)
 else:
+
+    # Select a large sample of duplicate pairs.
     # As the dataset grows, duplicate pairs become relatively more
-    # rare.  We need positive examples to train dedupe, so we have to
-    # signficantly increase the size of the sample compared to
+    # rare so we have to take a fairly large sample compared to
     # `csv_example.py`
     print 'selecting random sample from donors table...'
     data_sample = getSample(c, 750000, 'donor_id', 'donors')
@@ -113,31 +118,42 @@ else:
               'state': {'type': 'String'},
               'zip': {'type': 'String'},
               }
+
+    # Create a new deduper object and pass our data model to it.
     deduper = dedupe.Dedupe(fields)
 
-    # Sometimes we will want to add additional labeled examples to a
-    # training file. To do this can just load the existing labeled
-    # pairs...
+    # If we have training data saved from a previous run of dedupe,
+    # look for it an load it in.
+    # If you want to train from scratch, delete the training_file
     if os.path.exists(training_file):
         print 'reading labeled examples from ', training_file
         deduper.train(data_sample, training_file)
 
     print 'starting active labeling...'
     print 'finding uncertain pairs...'
-    # ... and then call training with our interactive function
+    
+    # Starts the trainin loop. Dedupe will find the next pair of records
+    # it is least certain about and ask you to label them as duplicates
+    # or not.
+    # use 'y', 'n' and 'u' keys to flag duplicates
+    # press 'f' when you are finished
     deduper.train(data_sample, dedupe.training.consoleLabel)
+
+    # When finished, save our training away to disk
     deduper.writeTraining(training_file)
 
 print 'blocking...'
+# Initialize our blocker, which determine our field weights and blocking 
+# predicates based on our training data
 blocker = deduper.blockingFunction(eta=0.001, epsilon=5)
+
+# Save our weights and predicates to disk.
+# If the settings file exists, we will skip all the training and learning
 deduper.writeSettings(settings_file)
 
 # ### Blocking on disk
 
-# So the learning is done and we have our blocker. However we cannot
-# block the data in memory. We have to pass through all the data and
-# create a blocking map table.
-
+# Iterate through all our input data and create a blocking map.
 # To run blocking on such a large set of data, we create a separate table
 # that contains blocking keys and record ids
 print 'creating blocking_map database'
@@ -148,17 +164,18 @@ c.execute("CREATE TABLE blocking_map "
 
 
 
-# First though, if we learned a TF-IDF blocking rule, we have to
-# create an TF-IDF canopies for the full data set.
+# We are using [TF/IDF](http://en.wikipedia.org/wiki/Tf%E2%80%93idf) as a 
+# potential predicate. 
+# If dedupe learned a TF-IDF blocking rule, we create go through the extra
+# step of creating TF-IDF canopies.
 print 'creating inverted index'
 c.execute(DONOR_SELECT + " LIMIT 10000")
 full_data = ((row['donor_id'], row) for row in c.fetchall())
 blocker.tfIdfBlocks(full_data)
 
 
-# Now, we are ready to block the data. We'll do this by creating
-# a generator that yields `(block_key, donor_id)` tuples. Dedupe guarantees
-# that these tuples will be unique
+# Next, we write our blocking map table by creating a generator that 
+# yields unique `(block_key, donor_id)` tuples.
 print 'writing blocking map'
 def block_data() :
     c.execute(DONOR_SELECT + " LIMIT 10000")
@@ -171,9 +188,8 @@ def block_data() :
 
 b_data = block_data()
 
-# MySQL, by default, has a hard limit on the size of a data object 
-# that can be passed to it. To get around this, we chunk the blocked data
-# in to groups of 10,000 blocks
+# MySQL has a hard limit on the size of a data object that can be passed to it. 
+# To get around this, we chunk the blocked data in to groups of 10,000 blocks
 step = 10000
 done = False
 while not done :
@@ -187,23 +203,20 @@ while not done :
   con.commit()
 
 
-# We create an index on the blocking key so that the `GROUP BY`
-# queries we will be making to cluster the data can  happen in a
-# reasonable time
+# Create an index on the blocking key for faster clustering
 print 'creating blocking map index. this will probably take a while ...'
 c.execute("CREATE INDEX blocking_map_key_idx ON blocking_map (block_key)")
 print 'created', time.time() - start_time, 'seconds'
 
-# ### Clustering from disk
+# ### Clustering
 
-# This grabs a block of records for comparison. We rely on the
-# ordering of the donor ids
+# Grabs a block of records for comparison.
 block_select = (DONOR_SELECT + 
                 " INNER JOIN blocking_map USING (donor_id) " 
                 "WHERE block_key = %s ORDER BY donor_id")
 
 
-# This generator yields blocks of data
+# Generator function that yields records based on blocking map keys
 def candidates_gen(block_keys) :
     for i, block_key in enumerate(block_keys) :
         if i % 10000 == 0 :
@@ -213,18 +226,17 @@ def candidates_gen(block_keys) :
         c.execute(block_select, (block_key,))
         yield ((row['donor_id'], row) for row in c.fetchall())
 
-# We grab all the block keys with more than one record associated with
-# it. These associated records will make up a block of records we will
-# compare within.
+# Grab all the block keys with more than one record.
+# These records will make up a block of records we will cluster.
 blocking_key_sql = "SELECT block_key, COUNT(*) AS num_candidates " \
                    "FROM blocking_map GROUP BY block_key HAVING num_candidates > 1"
 
-# Using a random sample of blocks we find a good threshold
+# Using a random sample of blocks we use goodThreshold find our clustering threshold
 c.execute(blocking_key_sql + " ORDER BY RAND() LIMIT 1000")
 sampled_block_keys = block_keys = (row['block_key'] for row in c.fetchall())
 threshold = deduper.goodThreshold(candidates_gen(sampled_block_keys))
 
-
+# With our found threshold, and candidates generator, perform the clustering operation
 c.execute(blocking_key_sql)
 block_keys = (row['block_key'] for row in c.fetchall())
 
@@ -234,9 +246,8 @@ clustered_dupes = deduper.duplicateClusters(candidates_gen(block_keys),
 
 # ### Writing out results
 
-# `duplicateClusters` gives us sequence of tuples of donor ids that
-# Dedupe believes all refer to the same entity. We write this out
-# onto an entity map table
+# We now have a sequence of tuples of donor ids that dedupe believes all 
+# refer to the same entity. We write this out onto an entity map table
 c.execute("DROP TABLE IF EXISTS entity_map")
 
 print 'creating entity_map database'
@@ -255,12 +266,14 @@ con.commit()
 
 c.execute("CREATE INDEX head_index ON entity_map (head_id)")
 con.commit()
-        
+
+# Print out the number of duplicates found
 print '# duplicate sets'
 print len(clustered_dupes)
 
+# Close our database connection
 c.close()
 con.close()
+
+# Print our run time
 print 'ran in', time.time() - start_time, 'seconds'
-
-
