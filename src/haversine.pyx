b75723aca16e3ba087351e66108f7bd1d85cd853
markhuberty <mark.huberty@gmail.com>
2013-03-05 14:29:57 -0600

    removed some extraneous debugging statements from the haversine routine

--- src/haversine.pyx
+++ src/haversine.pyx
@@ -7,13 +7,13 @@ import math
 
 
 cpdef float haversine(float lon1, float lat1, float lon2, float lat2):
-    print lon1, lat1, lon2, lat2
+
     rlon1, rlat1, rlon2, rlat2 = map(math.radians, [lon1, lat1, lon2, lat2])
     cdef float dlon = rlon2 - rlon1
     cdef float dlat = rlat2 - rlat1
 
     cdef float a = math.sin(dlat/2)**2 + math.cos(rlat1) * math.cos(rlat2) * math.sin(dlon/2)**2
-    print a
+
     cdef float c = 2 * math.asin(math.sqrt(a))
     # cdef float c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     cdef float km = 6371 * c
