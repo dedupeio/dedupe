fb0778c1ab84c50c77066f0a4fe5940e526b5282
Forest Gregg <fgregg@uchicago.edu>
2013-03-07 07:32:53 -0600

    fixed path:

--- dedupe/core.py
+++ dedupe/core.py
@@ -11,7 +11,7 @@ from itertools import count
 import numpy
 
 import lr
-from dedupe.affinegap import normalizedAffineGapDistance as stringDistance
+from dedupe.distance.affinegap import normalizedAffineGapDistance as stringDistance
 
 def randomPairs(n_records, sample_size, zero_indexed=True):
     '''Return random combinations of indicies for a square matrix of
