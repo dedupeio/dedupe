5902e5cc4f5b5ce7fe1a9fb115320518445489c9
nikitsaraf <nikit.saraf@gmail.com>
2013-08-31 01:32:27 +0530

    Fix another bug in ConstrainedClustering

--- dedupe/clustering.py
+++ dedupe/clustering.py
@@ -115,11 +115,11 @@ def clusterConstrained(dupes,threshold=.6):
             scored_pairs = numpy.asarray(biadjacency_matrix(sub_graph, row_order, col_order))
 
             scored_pairs[scored_pairs < threshold] = 0
-            scored_pairs = 1 - scored_pairs
-            row_prod = numpy.prod(scored_pairs, axis=0)
+            scored_pairs_temp = 1 - scored_pairs
+            row_prod = numpy.prod(scored_pairs_temp, axis=0)
             cost_matrix_row_prod = numpy.vstack((scored_pairs,numpy.tile(row_prod,(numpy.size(scored_pairs,1),1))))
-            col_prod = numpy.prod(scored_pairs, axis=1)
-            col_prod.shape = (numpy.size(scored_pairs,0),1)
+            col_prod = numpy.prod(scored_pairs_temp, axis=1)
+            col_prod.shape = (numpy.size(scored_pairs_temp,0),1)
             col_prod = numpy.vstack((col_prod,numpy.zeros((numpy.size(scored_pairs,1),1))))
             cost_matrix = numpy.hstack((cost_matrix_row_prod, numpy.tile(col_prod, (numpy.size(scored_pairs,0)))))
             cost_matrix = 1 - cost_matrix
