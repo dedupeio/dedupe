5dd9e27d02954500848919c7485fa9e255dd11c1
nikitsaraf <nikit.saraf@gmail.com>
2013-08-31 23:31:41 +0530

    Make test_predicates PEP8 Compliant

--- test/test_predicates.py
+++ test/test_predicates.py
@@ -1,6 +1,7 @@
 import unittest
 from dedupe import predicates
 
+
 class TestWholeSet(unittest.TestCase):
     def setUp(self):
         self.s1 = set(['red', 'blue', 'green'])
@@ -12,6 +13,7 @@ class TestWholeSet(unittest.TestCase):
     def test_empty_set(self):
         block_val = predicates.wholeSetPredicate(set())
         self.assertEqual(block_val, tuple())
+
 
 class TestSetElement(unittest.TestCase):
     def setUp(self):
@@ -25,21 +27,22 @@ class TestSetElement(unittest.TestCase):
         block_val = predicates.commonSetElementPredicate(set())
         self.assertEqual(block_val, tuple())
 
+
 class TestLatLongGrid(unittest.TestCase):
     def setUp(self):
         self.latlong1 = (42.335, -5.212)
-        self.latlong1grid = str( ((42.3, 42.4),(-5.3, -5.2)))
+        self.latlong1grid = str(((42.3, 42.4), (-5.3, -5.2)))
         self.latlong2 = (42, -5)
         self.latlong2grid = str(((41.9, 42.1), (-5.1, -4.9)))
 
     def test_precise_latlong(self):
         block_val = predicates.latLongGridPredicate(self.latlong1)
         self.assertEqual(self.latlong1grid, block_val)
-        
+
     def test_imprecise_latlong(self):
         block_val = predicates.latLongGridPredicate(self.latlong2)
         self.assertEqual(self.latlong2grid, block_val)
 
+
 if __name__ == '__main__':
     unittest.main()
-
