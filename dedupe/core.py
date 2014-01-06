22fd9eeac7e4cc543557dd1cec992c52c6d9e7f4
Forest Gregg <fgregg@uchicago.edu>
2014-01-05 21:16:27 -0600

    remove out old commented out code

--- dedupe/core.py
+++ dedupe/core.py
@@ -275,29 +275,3 @@ class frozendict(collections.Mapping):
         except AttributeError:
             h = self._cached_hash = hash(frozenset(self._d.iteritems()))
             return h
-
-
-
-
-# class frozendict(dict):
-#     def _blocked_attribute(obj):
-#         raise AttributeError, "A frozendict cannot be modified."
-
-#     def __new__(cls, *args):
-#         new = dict.__new__(cls)
-#         dict.__init__(new, *args)
-#         return new
-
-#     def __init__(self, *args):
-#         pass
-
-#     def __hash__(self):
-#         try:
-#             return self._cached_hash
-#         except AttributeError:
-#             h = self._cached_hash = hash(tuple(sorted(self.items())))
-#             return h
-
-#     def __repr__(self):
-#         return "frozendict(%s)" % dict.__repr__(self)
-
