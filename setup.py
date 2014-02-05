93e62f4c1ea550d2278258adeb9b7b4cf58e19cc
Forest Gregg <fgregg@uchicago.edu>
2014-02-05 11:32:35 -0600

    bump version number to 0.5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class NumpyExtension(Extension):
 setup(
     name='Dedupe',
     url='https://github.com/open-city/dedupe',
-    version='0.4',
+    version='0.5',
     packages=['dedupe', 'dedupe.distance'],
     ext_modules=[NumpyExtension('dedupe.distance.affinegap', ['src/affinegap.c']),
                  Extension('dedupe.distance.jaccard', ['src/jaccard.c']),
