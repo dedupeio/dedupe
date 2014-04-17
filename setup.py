99be22b27f90093af31ff28dccdfad693439d2e1
Derek Eder <derek.eder@gmail.com>
2014-04-17 14:49:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -70,12 +70,15 @@ setup(
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Information Analysis'],
     long_description="""
-    Deduplication, entity resolution, record linkage, author
-    disambiguation, and others ...
-    
-    As different research communities encountered this problem, they each
-    gave it a new name but, ultimately, its all about trying to figure out
-    what records are referring to the same thing.
+    *dedupe is a library that uses machine learning to perform de-duplication and entity resolution quickly on structured data.*
+
+    **dedupe** will help you: 
+
+    * **remove duplicate entries** from a spreadsheet of names and addresses
+    * **link a list** with customer information to another with order history, even without unique customer id's
+    * take a database of campaign contributions and **figure out which ones were made by the same person**, even if the names were entered slightly differently for each record
+
+    dedupe takes in human training data and comes up with the best rules for your dataset to quickly and automatically find similar records, even with very large databases.
     
     Important links:
     
