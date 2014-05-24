576721eb5d66309d70b60406bf6120ca5e22deff
Forest Gregg <fgregg@uchicago.edu>
2014-05-24 11:49:35 -0500

    fixe tests

--- tests/test_cosine.py
+++ tests/test_cosine.py
@@ -1,11 +1,4 @@
 import unittest
-<<<<<<< HEAD
-from dedupe.distance.cosine import CosineSetSimilarity
-import numpy
-
-
-class TestCosineClass(unittest.TestCase):
-=======
 from dedupe.distance.cosine import CosineSetSimilarity, CosineTextSimilarity
 import numpy
 
@@ -35,39 +28,25 @@ class TestSetCosineClass(unittest.TestCase):
         self.assertAlmostEqual(cosine_sim, 1, places=5)
 
 class TestTextCosineClass(unittest.TestCase):
->>>>>>> add test for cosine text
     def setUp(self):
         self.ilist = ['a b c',
                       'b c d',
                       'd e f']
 
     def test_cosine(self):
-<<<<<<< HEAD
-        cosine = CosineSetSimilarity(self.ilist)
-=======
         cosine = CosineTextSimilarity(self.ilist)
->>>>>>> add test for cosine text
         s1 = self.ilist[0]
         s2 = self.ilist[1]
         cosine_sim = cosine(s1, s2)
         self.assertAlmostEqual(cosine_sim, 0.378, places=3)
-
-    def test_cosine_na(self):
-<<<<<<< HEAD
-        cosine = CosineSetSimilarity(self.ilist)
-        cosine_sim = cosine(self.ilist[0], frozenset([]))
-        assert numpy.isnan(cosine_sim)
         
-    def test_cosine_identical(self):
-        cosine = CosineSetSimilarity(self.ilist)
-=======
+    def test_cosine_na(self):
         cosine = CosineTextSimilarity(self.ilist)
         cosine_sim = cosine(self.ilist[0], '')
         assert numpy.isnan(cosine_sim)
         
     def test_cosine_identical(self):
         cosine = CosineTextSimilarity(self.ilist)
->>>>>>> add test for cosine text
         cosine_sim = cosine(self.ilist[0], self.ilist[0])
         self.assertAlmostEqual(cosine_sim, 1, places=5)
 
