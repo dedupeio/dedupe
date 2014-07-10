6fbb93f98c3f200e4d2f5db73a846fd8bf15c317
Cathy Deng <cathyd125@gmail.com>
2014-07-10 16:25:10 -0500

    tweak getCentroid, modify getCanonicalRep so that it takes in data model, add to tests

--- dedupe/centroid.py
+++ dedupe/centroid.py
@@ -1,9 +1,6 @@
 import numpy as np
 from numpy import matlib
 
-#this is the string distance calculation
-from dedupe.distance.affinegap import normalizedAffineGapDistance as comparator
-
 #Canonicalization###########################################################################
 
 # takes in a list of attribute values for a field,
@@ -12,25 +9,19 @@ from dedupe.distance.affinegap import normalizedAffineGapDistance as comparator
 def getCentroid( attribute_variants, comparator ):
     n = len(attribute_variants)
     # if all values were empty & ignored, return ''
-    if n == 0:
-        return ''
-    if n == 1:
-        return attribute_variants[0]
     dist_matrix = np.zeros([n,n])
     # this is a matrix of distances between all strings
     # populate distance matrix by looping through elements of matrix triangle
-    for i in range (1,n):
+    for i in range (0,n):
         for j in range (0, i):
             dist = comparator(attribute_variants[i], attribute_variants[j])
             dist_matrix[i,j] = dist
             dist_matrix[j,i] = dist
     # find avg distance per string
     avg_dist = dist_matrix.mean(0)
-    print avg_dist
     # find string with min avg distance
     
     min_dist_indices = np.where(avg_dist==avg_dist.min())[0]
-    print min_dist_indices
     # if there is only one value w/ min avg dist
     if len(min_dist_indices)==1:
         centroid_index = min_dist_indices[0]
@@ -45,25 +36,22 @@ def getCentroid( attribute_variants, comparator ):
 def breakCentroidTie( attribute_variants, min_dist_indices ):
     return attribute_variants[min_dist_indices[0]]
 
-#this is the string distance calculation
-from dedupe.distance.affinegap import normalizedAffineGapDistance as comparator
-
 # takes in a cluster of duplicates & data, returns canonical representation of cluster
 # TO-DO: get this to take in data model, so that it knows data types, comparators
-def getCanonicalRep( dupe_cluster, data_d):
-    keys = data_d[0].keys()
+def getCanonicalRep( dupe_cluster, data_d, data_model):
     canonical_rep = dict()
-    ####### TO-DO ############
-    # comparator = 
 
     #loop through keys & values in data, get centroid for each key
-    for key in keys:
+    for key, comparator in data_model.field_comparators.items():
         key_values = []
         for record_id in dupe_cluster :
             #ignore empty values (assume non-empty values always better than empty value for canonical record)
             if data_d[record_id][key] != '':
                 key_values.append(data_d[record_id][key])
-        canonical_rep[key] = getCentroid(key_values, comparator)
+        if key_values:
+            canonical_rep[key] = getCentroid(key_values, comparator)
+        else:
+            canonical_rep[key] = ''
     return canonical_rep
 
 #############################################################################################
--- tests/test_canonical.py
+++ tests/test_canonical.py
@@ -5,13 +5,30 @@ import numpy
 import random
 import warnings
 
-from dedupe.distance.affinegap import normalizedAffineGapDistance as comparator
 
 class CanonicalizationTest(unittest.TestCase) :
+
+	def setUp(self) :
+		self.data_d = { 1 : {"name": "mary crane", "address": "123 main st"}, 
+					 	2 : {"name": "mary crane east", "address": "123 main street"}, 
+						3 : {"name": "mary crane west", "address": "123 man st"}}
+		deduper = dedupe.Dedupe({'name': {'type': 'String'},
+                      			 'address':   {'type': 'String'}})
+		self.data_model = deduper.data_model
+
+
 	def test_get_centroid(self) :
+		from dedupe.distance.affinegap import normalizedAffineGapDistance as comparator
 		attributeList = ['mary crane center', 'mary crane center north', 'mary crane league - mary crane - west', 'mary crane league mary crane center (east)', 'mary crane league mary crane center (north)', 'mary crane league mary crane center (west)', 'mary crane league - mary crane - east', 'mary crane family and day care center', 'mary crane west', 'mary crane center east', 'mary crane league mary crane center (east)', 'mary crane league mary crane center (north)', 'mary crane league mary crane center (west)', 'mary crane league', 'mary crane', 'mary crane east 0-3', 'mary crane north', 'mary crane north 0-3', 'mary crane league - mary crane - west', 'mary crane league - mary crane - north', 'mary crane league - mary crane - east', 'mary crane league - mary crane - west', 'mary crane league - mary crane - north', 'mary crane league - mary crane - east']
 		centroid = dedupe.centroid.getCentroid (attributeList, comparator)
 		assert centroid == 'mary crane'
+
+	def test_get_canonical_rep(self) :
+		rep = dedupe.centroid.getCanonicalRep((1,2,3), self.data_d, self.data_model)
+		assert rep == {'name': 'mary crane', 'address': '123 main street'}
+
+
+
 
 if __name__ == "__main__":
 	unittest.main()
