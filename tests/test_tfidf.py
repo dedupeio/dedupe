10a86d7f9a2add225d86f42342c2f2ba2167f8ff
Forest Gregg <fgregg@uchicago.edu>
2014-08-29 12:52:28 -0500

    EOF tests

--- tests/test_tfidf.py
+++ tests/test_tfidf.py
@@ -9,7 +9,8 @@ class ParsingTest(unittest.TestCase) :
         assert self.index._parseTerms(u'`AND OR NOT ( ) ATOM \\EOF, xx') ==\
             ['xxAND', 'xxOR', 'xxNOT', 'xxATOM', 'xxEOF', 'xx']
         
-        
+        self.index.index(1, 'AND OR EOF NOT')
+        assert self.index.search('AND OR EOF NOT')[0] == 1
 
 if __name__ == "__main__":
     unittest.main()
