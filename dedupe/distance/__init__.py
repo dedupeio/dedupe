da8488f6e0f0a48234895716acef08b8d946f2e3
Forest Gregg <fgregg@uchicago.edu>
2014-11-19 17:34:51 -0600

    stray references of comparelatlong

--- dedupe/distance/__init__.py
+++ dedupe/distance/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import affinegap
-from haversine import compareLatLong
 from cosine import CosineTextSimilarity, CosineSetSimilarity
 from categorical import CategoricalComparator
--- dedupe/fieldclasses.py
+++ dedupe/fieldclasses.py
@@ -125,7 +125,7 @@ class LatLongType(FieldType) :
 
     _predicate_functions = [predicates.latLongGridPredicate]
 
-    @static
+    @staticmethod
     def comparator(field_1, field_2) :
         if field_1 == (0.0,0.0) or field_2 == (0.0,0.0) :
             return numpy.nan
