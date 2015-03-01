faeec34095e86755751614c7701069f095f5d6a3
Forest Gregg <fgregg@uchicago.edu>
2015-03-01 17:27:42 -0600

    bump version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 setup(
     name='dedupe',
     url='https://github.com/datamade/dedupe',
-    version='0.7.7.1.2',
+    version='0.7.7.1.3',
     description='A python library for accurate and scaleable data deduplication and entity-resolution',
     packages=['dedupe', 'dedupe.variables'],
     ext_modules=[Extension('dedupe.cpredicates', ['src/cpredicates.c'])],
