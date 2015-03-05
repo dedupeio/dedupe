808dc347ec3c15a907546aabf67dffb246dcbc1d
Forest Gregg <fgregg@uchicago.edu>
2015-03-05 14:48:19 -0600

    refactor strings

--- dedupe/variables/string.py
+++ dedupe/variables/string.py
@@ -4,12 +4,7 @@ from dedupe import predicates
 from affinegap import normalizedAffineGapDistance
 from simplecosine.cosine import CosineTextSimilarity, CosineSetSimilarity
 
-class ShortStringType(FieldType) :
-    comparator = normalizedAffineGapDistance
-    type = "ShortString"
-
-    _predicate_functions = (predicates.wholeFieldPredicate,
-                            predicates.tokenFieldPredicate,
+base_predicate_functions = (predicates.wholeFieldPredicate,
                             predicates.firstTokenPredicate,
                             predicates.commonIntegerPredicate,
                             predicates.nearIntegersPredicate,
@@ -17,42 +12,65 @@ class ShortStringType(FieldType) :
                             predicates.sameThreeCharStartPredicate,
                             predicates.sameFiveCharStartPredicate,
                             predicates.sameSevenCharStartPredicate,
-                            predicates.commonFourGram,
-                            predicates.commonSixGram,
-                            predicates.suffixArray,
                             predicates.commonTwoTokens,
                             predicates.commonThreeTokens,
                             predicates.fingerprint,
                             predicates.oneGramFingerprint,
                             predicates.twoGramFingerprint,
-                            predicates.sortedAcronym,
-                            predicates.doubleMetaphone,
-                            predicates.metaphoneToken)
+                            predicates.sortedAcronym)
 
-class StringType(ShortStringType) :
-    comparator = normalizedAffineGapDistance
-    type = "String"
-
-    _canopy_thresholds = (0.2, 0.4, 0.6, 0.8)
+class IndexNGramString(object) :
 
     def __init__(self, definition) :
-        super(StringType, self).__init__(definition)
-
-        self.predicates += [predicates.TfidfTextPredicate(threshold, 
-                                                          self.field)
-                            for threshold in self._canopy_thresholds]
+        super(IndexNGramString, self).__init__(definition)
 
         self.predicates += [predicates.TfidfNGramPredicate(threshold, 
                                                           self.field)
                             for threshold in self._canopy_thresholds]
 
 
+class IndexTextString(object) :
 
-class TextType(StringType) :
+    def __init__(self, definition) :
+        super(IndexTextString, self).__init__(definition)
+
+        self.predicates += [predicates.TfidfTextPredicate(threshold, 
+                                                          self.field)
+                            for threshold in self._canopy_thresholds]
+
+
+
+
+class ShortStringType(IndexNGramString, FieldType) :
+    comparator = normalizedAffineGapDistance
+    type = "ShortString"
+
+    _canopy_thresholds = (0.2, 0.4, 0.6, 0.8)
+
+    _predicate_functions = (base_predicate_functions 
+                            + (predicates.commonFourGram,
+                               predicates.commonSixGram,
+                               predicates.tokenFieldPredicate,
+                               predicates.suffixArray,
+                               predicates.doubleMetaphone,
+                               predicates.metaphoneToken))
+
+
+class StringType(IndexTextString, ShortStringType) :
+    comparator = normalizedAffineGapDistance
+    type = "String"
+
+
+
+class TextType(IndexTextString, FieldType) :
     type = "Text"
+
+    _predicate_functions = base_predicate_functions 
+    _canopy_thresholds = (0.2, 0.4, 0.6, 0.8)
 
     def __init__(self, definition) :
         super(TextType, self).__init__(definition)
+
 
         if 'corpus' not in definition :
             definition['corpus'] = []
