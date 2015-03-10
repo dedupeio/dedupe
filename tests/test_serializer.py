c342e1e7613a58b289ace53b5513d40a75376f42
Forest Gregg <fgregg@uchicago.edu>
2015-03-10 11:12:09 -0500

    clean up

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -15,33 +15,36 @@ class SerializerTest(unittest.TestCase) :
         if sys.version < '3' :
             from StringIO import StringIO
             output = StringIO()
-            encoded_file = codecs.EncodedFile(output, data_encoding='utf8', file_encoding='ascii')
+            encoded_file = codecs.EncodedFile(output, 
+                                              data_encoding='utf8', 
+                                              file_encoding='ascii')
         else :
             from io import StringIO
             encoded_file = StringIO()
 
         training_pairs = OrderedDict(
             {u"distinct":[
-                (dedupe.core.frozendict(OrderedDict(((u'bar', frozenset([u'barë'])),
-                                                                                         (u'foo', u'baz')))), 
+                (dedupe.core.frozendict(OrderedDict(((u'bar', 
+                                                      frozenset([u'barë'])),
+                                                    (u'foo', u'baz')))), 
                  dedupe.core.frozendict({u'foo' : u'baz'}))], 
              u"match" : []})
 
         json.dump(training_pairs, 
-                            encoded_file, 
-                            default=dedupe.serializer._to_json,
-                            ensure_ascii = True)
+                  encoded_file, 
+                  default=dedupe.serializer._to_json,
+                  ensure_ascii = True)
 
         encoded_file.seek(0)
 
-        loaded_training_pairs = json.load(encoded_file, 
-                                                                            cls=dedupe.serializer.dedupe_decoder)
+        loaded_training_pairs = json.load(encoded_file,
+                                          cls=dedupe.serializer.dedupe_decoder)
 
         assert loaded_training_pairs["distinct"][0] ==\
-                training_pairs["distinct"][0]
+            training_pairs["distinct"][0]
 
         assert isinstance(loaded_training_pairs["distinct"][0][0]["bar"], 
-                                            frozenset)
+                          frozenset)
 
         deduper = dedupe.Dedupe([{'field' : 'foo', 'type' : 'String'}])
 
