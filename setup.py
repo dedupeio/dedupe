63923000d5c7392a0a6c3f0dcb5131d98a3b222c
Forest Gregg <fgregg@uchicago.edu>
2015-08-28 13:09:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ install_requires=['fastcluster',
                   'zope.interface', 
                   'BTrees>=4.1.4',
                   'simplejson',
-                  'numpy>=1.9.2',
                   'zope.index']
 
 setup(
@@ -32,7 +31,6 @@ setup(
     ext_modules=[Extension('dedupe.cpredicates', ['src/cpredicates.c'])],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
     install_requires=install_requires,
-    setup_requires=['numpy>=1.9.2'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
