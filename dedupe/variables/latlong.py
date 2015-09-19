43823ebea57e4d69c41d6c970942d1a3cf250a44
Forest Gregg <fgregg@uchicago.edu>
2015-09-19 07:14:36 -0500

    prepare for Nones as missing

--- dedupe/variables/latlong.py
+++ dedupe/variables/latlong.py
@@ -12,7 +12,7 @@ class LatLongType(FieldType) :
     @staticmethod
     def comparator(field_1, field_2) :
         if field_1 == (0.0,0.0) or field_2 == (0.0,0.0) :
-            warnings.warn('In the 1.2 release, missing data will have a value of None')
+            warnings.warn('In the dedupe 1.2 release, missing data will have to have a value of None. See http://dedupe.readthedocs.org/en/latest/Variable-definition.html#missing-data')
             return numpy.nan
         else :
             return haversine(field_1, field_2)
