ff9fa3c50a26dc49d1abfe29e9dd34b2583e2c0d
Forest Gregg <fgregg@uchicago.edu>
2016-10-24 14:40:55 -0500

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ try:
 except ImportError :
     raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
 
-install_requires=['fastcluster', 
+install_requires=['fastcluster',
                   'dedupe-hcluster',
                   'affinegap>=1.3',
                   'categorical-distance>=1.9',
@@ -24,7 +24,7 @@ install_requires=['fastcluster',
 setup(
     name='dedupe',
     url='https://github.com/datamade/dedupe',
-    version='1.5.0',
+    version='1.5.1',
     author='Forest Gregg',
     author_email='fgregg@datamade.us',
     description='A python library for accurate and scaleable data deduplication and entity-resolution',
@@ -49,16 +49,16 @@ setup(
     long_description="""
     *dedupe is a library that uses machine learning to perform de-duplication and entity resolution quickly on structured data.*
 
-    **dedupe** will help you: 
+    **dedupe** will help you:
 
     * **remove duplicate entries** from a spreadsheet of names and addresses
     * **link a list** with customer information to another with order history, even without unique customer id's
     * take a database of campaign contributions and **figure out which ones were made by the same person**, even if the names were entered slightly differently for each record
 
     dedupe takes in human training data and comes up with the best rules for your dataset to quickly and automatically find similar records, even with very large databases.
-    
+
     Important links:
-    
+
     * Documentation: https://dedupe.readthedocs.io/
     * Repository: https://github.com/datamade/dedupe
     * Issues: https://github.com/datamade/dedupe/issues
