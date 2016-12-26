4f53b829377c6261c28c8a77b3ee321dbb42be5e
Forest Gregg <fgregg@uchicago.edu>
2016-12-26 14:30:37 -0600

    disable multiprocessing if numpy linked to MKL

--- dedupe/backport.py
+++ dedupe/backport.py
@@ -20,6 +20,10 @@ if "accelerate" in config_info or "veclib" in config_info :
                   "Multiprocessing will be disabled."
                   " http://mail.scipy.org/pipermail/numpy-discussion/2012-August/063589.html")
     MULTIPROCESSING = False
+elif "mkl_core" in config_info:
+    warnings.warn("Numpy linked against MKL. "
+                  "Multiprocessing will be disabled. https://github.com/joblib/joblib/issues/138")
+    MULTIPROCESSING = False
 elif platform.system() == 'Windows' :
     warnings.warn("Dedupe does not currently support multiprocessing on Windows")
     MULTIPROCESSING = False
