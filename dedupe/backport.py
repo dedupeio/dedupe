f5213d79b10333aabe353da6e3ae1cca5802b17e
Forest Gregg <fgregg@uchicago.edu>
2016-12-26 14:44:02 -0600

    remove unused signature code from backport

--- dedupe/backport.py
+++ dedupe/backport.py
@@ -4,8 +4,6 @@ import threading
 import warnings
 import platform
 import sys
-import inspect
-from collections import OrderedDict
 
 from future.utils import viewitems
 
@@ -40,86 +38,4 @@ else :
     from multiprocessing.dummy import Process, Pool, Queue
     SimpleQueue = Queue
 
-
-# This backport code comes from https://gerrit.wikimedia.org/r/#/c/229683/1/pywikibot/tools/__init__.py,cm
-# (C) Pywikibot team, 2008-2015
-#
-# Distributed under the terms of the MIT license.
-
-try:
-    Parameter = inspect.Parameter
-    Signature = inspect.Signature
-
-    def signature(obj):
-        """Return the signature for callable and None if a ValueError occurs."""
-        try:
-            return inspect.signature(obj)
-        except ValueError:
-            return None
-except:
-    # A basic implementation of the Parameter and Signature class to provide
-    # the same support for getargspec on versions before 3.3
-
-    class _empty:
-
-        """A dummy class to show that the Parameter has no default value."""
-
-        pass
-
-    class Parameter(object):
-
-        """A basic Parameter class implementation."""
-
-        POSITIONAL_ONLY = 0
-        POSITIONAL_OR_KEYWORD = 1
-        VAR_POSITIONAL = 2
-        KEYWORD_ONLY = 3
-        VAR_KEYWORD = 4
-
-        empty = _empty
-
-        def __init__(self, name, kind, default=_empty):
-            """Constructor."""
-            self.name = name
-            self.kind = kind
-            self.default = default
-
-        def replace(self, **kwargs):
-            """Create a copy and replace the given variables."""
-            name = kwargs.pop('name', self.name)
-            kind = kwargs.pop('kind', self.kind)
-            default = kwargs.pop('default', self.default)
-            assert not kwargs
-            return Parameter(name, kind, default=default)
-
-    class Signature(object):
-
-        """A basic Signature class implementation."""
-
-        def __init__(self, parameters=None):
-            """Constructor."""
-            self.parameters = OrderedDict()
-            for param in parameters or []:
-                self.parameters[param.name] = param
-
-    def signature(obj):
-        """Return the basic signature for the callable."""
-        try:
-            spec = inspect.getargspec(obj)
-        except TypeError:
-            return None
-        defaults = (_empty,) * len(spec[0])
-        if spec[3] is not None:
-            # spec[3] are the defaults counting from the end, add to the
-            # "non-existing" defaults and shift by number of params
-            defaults = (defaults + spec[3])[-len(spec[0]):]
-        parameters = []
-        for arg, default in zip(spec[0], defaults):
-            parameters += [Parameter(arg, Parameter.POSITIONAL_OR_KEYWORD,
-                                     default=default)]
-        if spec[1] is not None:
-            parameters += [Parameter(spec[1], Parameter.VAR_POSITIONAL)]
-        if spec[2] is not None:
-            parameters += [Parameter(spec[2], Parameter.VAR_KEYWORD)]
-        return Signature(parameters)
 
