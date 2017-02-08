13efd3a00e49aadf95fe6745ccc84c00977419b2
Jean Cochrane <jean@jeancochrane.com>
2017-02-08 11:38:35 -0600

    Puttering around

--- .install-dependencies.sh
+++ .install-dependencies.sh
@@ -4,7 +4,7 @@ set -e -x
 
 pip install --upgrade pip
 pip install wheel
-pip install numpy>=1.9.2
+pip install "numpy>=1.9.2"
 pip install --upgrade -r requirements.txt
 pip install coveralls
 cython src/*.pyx
