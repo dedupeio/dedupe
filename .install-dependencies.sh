44b02b3e3d83abe93bb0caa9ad819dd3bf70c5ea
Jean Cochrane <jean@jeancochrane.com>
2017-02-08 10:09:43 -0600

    Move travis installation to a separate script

---
+++ .install-dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e -x
+
+pip install --upgrade pip
+pip install wheel
+pip install numpy>=1.9.2
+pip install --upgrade -r requirements.txt
+pip install coveralls
+cython src/*.pyx
+pip install -e .
