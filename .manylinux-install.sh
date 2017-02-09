bbc98334c821cb5a1819e68c31b87b9938d18f90
Forest Gregg <fgregg@users.noreply.github.com>
2017-02-09 10:41:48 -0600

    Provide manylinux PyPi builds (#514)
    * start on manylinux road
    
    * start on manylinux road
    
    * start on manylinux road
    
    * start on manylinux road
    
    * start on manylinux road
    
    * Move travis installation to a separate script
    
    * Move travis installation to a separate script
    
    * Trying to fix exit status 127
    
    * Poking around error
    
    * Travis pulled the wrong commit? Trying again
    
    * Puttering around
    
    * Sourcing works! Trying with docker image
    
    * Try different bash location
    
    * Whoops, don't try to mount the image
    
    * What is the dang directory structure
    
    * Try commands closer to the manylinux example
    
    * Finalize the travis architecture
    
    * Linted travis file
    
    * Limit the manylinux Python versions
    
    * Full path to Cython
    
    * Only build dedupe wheels
    
    * Use the dedupe wheels for testing
    
    * Force dedupe wheel to re-install for tests
    
    * Run tests from the right directory
    
    * Could directory location be the problem?
    
    * Gonna need to be in the right directory first
    
    * Fix breaking test
    
    * Tests pass, now fixing temp files
    
    * Pls don't upload to PyPi from untagged commits 🙈
    
    * Trying to appease the YAML gods
    
    * Appeasing the YAML Gods, pt 2

---
+++ .manylinux-install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    if [[ "${PYBIN}" == *"cp27"* ]] || [[ "${PYBIN}" == *"cp34"* ]] || [[ "${PYBIN}" == *"cp35"* ]]; then
+        "${PYBIN}/pip" install "numpy>=1.9.2"
+        "${PYBIN}/pip" install -r /io/requirements.txt
+        "${PYBIN}/pip" install coveralls
+        "${PYBIN}/cython" /io/src/*.pyx
+        "${PYBIN}/pip" install -e /io/
+        "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/dedupe*.whl; do
+    if  [[ "${whl}" != *"dedupe_hcluster"* ]]; then
+        auditwheel repair "$whl" -w /io/wheelhouse/
+    fi
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin; do
+    if [[ "${PYBIN}" == *"cp27"* ]] || [[ "${PYBIN}" == *"cp34"* ]] || [[ "${PYBIN}" == *"cp35"* ]]; then
+        "${PYBIN}/pip" uninstall -y dedupe
+        "${PYBIN}/pip" install dedupe --no-index -f /io/wheelhouse
+        "${PYBIN}/pytest" /io/tests --cov dedupe
+        cd /io/
+        "${PYBIN}/python" tests/canonical.py -vv
+        rm canonical_learned_settings
+        cd /
+    fi
+done
+
+# If everything works, upload wheels to PyPi
+travis=$( cat /io/.travis_tag )
+PYBIN34="/opt/python/cp34-cp34m/bin"
+if [[ $travis ]]; then
+    "${PYBIN34}/pip" install twine;
+    "${PYBIN34}/twine" upload --config-file /io/.pypirc /io/wheelhouse/dedupe*.whl;
+fi
