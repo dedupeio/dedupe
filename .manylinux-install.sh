2cd1a1e70187d36ffada959da6beaa73ab4b01f2
Forest Gregg <fgregg@uchicago.edu>
2017-03-24 22:04:00 -0500

    3.6 manylinux

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -4,7 +4,7 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" == *"cp27"* ]] || [[ "${PYBIN}" == *"cp34"* ]] || [[ "${PYBIN}" == *"cp35"* ]]; then
+    if [[ "${PYBIN}" == *"cp27"* ]] || [[ "${PYBIN}" == *"cp34"* ]] || [[ "${PYBIN}" == *"cp35"* ]] || [[ "${PYBIN}" == *"cp36"* ]]; then
         "${PYBIN}/pip" install "numpy>=1.9.2"
         "${PYBIN}/pip" install -r /io/requirements.txt
         "${PYBIN}/cython" /io/src/*.pyx
@@ -22,7 +22,7 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" == *"cp27"* ]] || [[ "${PYBIN}" == *"cp34"* ]] || [[ "${PYBIN}" == *"cp35"* ]]; then
+    if [[ "${PYBIN}" == *"cp27"* ]] || [[ "${PYBIN}" == *"cp34"* ]] || [[ "${PYBIN}" == *"cp35"* ]] || [[ "${PYBIN}" == *"cp36"* ]]; then
         "${PYBIN}/pip" uninstall -y dedupe
         "${PYBIN}/pip" install dedupe --no-index -f /io/wheelhouse
         "${PYBIN}/pytest" /io/tests
