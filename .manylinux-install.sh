871c61563848ac822ffee38d9b496607d0e45082
Forest Gregg <fgregg@uchicago.edu>
2017-04-25 09:21:11 -0500

    don't run tests on manylinux

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -10,6 +10,7 @@ for PYBIN in /opt/python/*/bin; do
         "${PYBIN}/cython" /io/src/*.pyx
         "${PYBIN}/pip" install -e /io/
         "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+        rm -rf /io/build /io/*.egg-info
     fi
 done
 
@@ -17,15 +18,6 @@ done
 for whl in wheelhouse/dedupe*.whl; do
     if  [[ "${whl}" != *"dedupe_hcluster"* ]]; then
         auditwheel repair "$whl" -w /io/wheelhouse/
-    fi
-done
-
-# Install packages and test
-for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" == *"cp27"* ]] || [[ "${PYBIN}" == *"cp34"* ]] || [[ "${PYBIN}" == *"cp35"* ]] || [[ "${PYBIN}" == *"cp36"* ]]; then
-        "${PYBIN}/pip" uninstall -y dedupe
-        "${PYBIN}/pip" install dedupe --no-index -f /io/wheelhouse
-        "${PYBIN}/pytest" /io/tests
     fi
 done
 
