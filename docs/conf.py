31180a2f92568e13c6154d869eb0d3c05c42c8f0
Jean Cochrane <jean@jeancochrane.com>
2017-05-12 12:09:16 -0500

    Remove canonical_url flag, now that the right URL is indexed

--- docs/conf.py
+++ docs/conf.py
@@ -116,9 +116,9 @@ html_style = 'css/custom.css'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'canonical_url': 'https://dedupe.io/developers/library/'
-}
+# html_theme_options = {
+    # 'canonical_url': 'https://dedupe.io/developers/library/'
+# }
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
