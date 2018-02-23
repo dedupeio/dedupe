fbe9cb8892fd3a277b92497f09772ee54fab5da2
Forest Gregg <fgregg@uchicago.edu>
2018-02-22 22:16:33 -0600

    flake8 core.py

--- dedupe/core.py
+++ dedupe/core.py
@@ -3,15 +3,6 @@
 from builtins import range, next, zip, map
 from future.utils import viewvalues
 import sys
-if sys.version < '3':
-    text_type = unicode
-    binary_type = str
-    int_type = long
-else:
-    text_type = str
-    binary_type = bytes
-    unicode = str
-    int_type = int
 
 import itertools
 import time
@@ -23,14 +14,26 @@ import collections
 import warnings
 import functools
 
-
 import numpy
 
-class ChildProcessError(Exception) :
+if sys.version < '3':
+    text_type = unicode  # noqa: F821
+    binary_type = str
+    int_type = long  # noqa: F821
+else:
+    text_type = str
+    binary_type = bytes
+    unicode = str
+    int_type = int
+
+
+class ChildProcessError(Exception):
     pass
+
 
 class BlockingError(Exception):
     pass
+
 
 def randomPairs(n_records, sample_size):
     """
@@ -41,7 +44,7 @@ def randomPairs(n_records, sample_size):
     """
     n = int(n_records * (n_records - 1) / 2)
 
-    if sample_size >= n :
+    if sample_size >= n:
         random_pairs = numpy.arange(n, dtype='uint')
     else:
         try:
@@ -49,14 +52,16 @@ def randomPairs(n_records, sample_size):
                                        dtype='uint')
         except OverflowError:
             return randomPairsWithReplacement(n_records, sample_size)
-            
-    
+
     b = 1 - 2 * n_records
 
-    i = numpy.floor((-b - 2 * numpy.sqrt(2 * (n - random_pairs) + 0.25)) / 2).astype('uint')
+    root = (-b - 2 * numpy.sqrt(2 * (n - random_pairs) + 0.25)) / 2
+
+    i = numpy.floor(root).astype('uint')
     j = numpy.rint(random_pairs + i * (b + i + 2) / 2 + 1).astype('uint')
 
     return zip(i, j)
+
 
 def randomPairsMatch(n_records_A, n_records_B, sample_size):
     """
@@ -74,75 +79,76 @@ def randomPairsMatch(n_records_A, n_records_B, sample_size):
 
     return zip(i, j)
 
-def randomPairsWithReplacement(n_records, sample_size) :
+
+def randomPairsWithReplacement(n_records, sample_size):
     # If the population is very large relative to the sample
     # size than we'll get very few duplicates by chance
     warnings.warn("There may be duplicates in the sample")
 
-    try :
-        random_indices = numpy.random.randint(n_records, 
-                                              size=sample_size*2)
+    try:
+        random_indices = numpy.random.randint(n_records,
+                                              size=sample_size * 2)
     except (OverflowError, ValueError):
         max_int = numpy.iinfo('int').max
         warnings.warn("Asked to sample pairs from %d records, will only sample pairs from first %d records" % (n_records, max_int))
-        random_indices = numpy.random.randint(max_int, 
-                                              size=sample_size*2)
 
-        
+        random_indices = numpy.random.randint(max_int,
+                                              size=sample_size * 2)
+
     random_indices = random_indices.reshape((-1, 2))
     random_indices.sort(axis=1)
 
     return [(p.item(), q.item()) for p, q in random_indices]
 
 
-class ScoreDupes(object) :
-    def __init__(self, data_model, classifier, threshold) :
+class ScoreDupes(object):
+    def __init__(self, data_model, classifier, threshold):
         self.data_model = data_model
         self.classifier = classifier
         self.threshold = threshold
         self.score_queue = None
 
-    def __call__(self, records_queue, score_queue) :
+    def __call__(self, records_queue, score_queue):
         self.score_queue = score_queue
-        while True :
+        while True:
             record_pairs = records_queue.get()
-            if record_pairs is None :
+            if record_pairs is None:
                 break
 
-            try :
+            try:
                 filtered_pairs = self.fieldDistance(record_pairs)
-                if filtered_pairs is not None :
+                if filtered_pairs is not None:
                     score_queue.put(filtered_pairs)
-            except Exception as e :
+            except Exception as e:
                 score_queue.put(e)
                 raise
 
         score_queue.put(None)
 
-    def fieldDistance(self, record_pairs) :
+    def fieldDistance(self, record_pairs):
         ids = []
         records = []
-        
-        for record_pair in record_pairs :
-            ((id_1, record_1, smaller_ids_1), 
+
+        for record_pair in record_pairs:
+            ((id_1, record_1, smaller_ids_1),
              (id_2, record_2, smaller_ids_2)) = record_pair
 
-            if smaller_ids_1.isdisjoint(smaller_ids_2) :
-                
+            if smaller_ids_1.isdisjoint(smaller_ids_2):
+
                 ids.append((id_1, id_2))
                 records.append((record_1, record_2))
 
-        if records :
-            
+        if records:
+
             distances = self.data_model.distances(records)
-            scores = self.classifier.predict_proba(distances)[:,-1]
+            scores = self.classifier.predict_proba(distances)[:, -1]
 
             mask = scores > self.threshold
             if mask.any():
                 id_type = sniff_id_type(ids)
                 ids = numpy.array(ids, dtype=id_type)
 
-                dtype = numpy.dtype([('pairs', id_type, 2), 
+                dtype = numpy.dtype([('pairs', id_type, 2),
                                      ('score', 'f4', 1)])
 
                 temp_file, file_path = tempfile.mkstemp()
@@ -157,18 +163,19 @@ class ScoreDupes(object) :
 
                 return file_path, dtype
 
-def mergeScores(score_queue, result_queue, stop_signals) :
+
+def mergeScores(score_queue, result_queue, stop_signals):
     scored_pairs_file, file_path = tempfile.mkstemp()
     os.close(scored_pairs_file)
 
     seen_signals = 0
     end = 0
 
-    while seen_signals < stop_signals  :
+    while seen_signals < stop_signals:
 
         score_chunk = score_queue.get()
 
-        if isinstance(score_chunk, Exception) :
+        if isinstance(score_chunk, Exception):
             result_queue.put(score_chunk)
             raise
         elif score_chunk is None:
@@ -195,11 +202,12 @@ def mergeScores(score_queue, result_queue, stop_signals) :
     else:
         result_queue.put(None)
 
-def scoreDuplicates(records, data_model, classifier, num_cores=1, threshold=0) :
-    if num_cores < 2 :
+
+def scoreDuplicates(records, data_model, classifier, num_cores=1, threshold=0):
+    if num_cores < 2:
         from multiprocessing.dummy import Process, Queue
         SimpleQueue = Queue
-    else :
+    else:
         from .backport import Process, SimpleQueue, Queue
 
     first, records = peek(records)
@@ -209,7 +217,7 @@ def scoreDuplicates(records, data_model, classifier, num_cores=1, threshold=0) :
                             "the data you trained on?")
 
     record_pairs_queue = Queue(2)
-    score_queue =  SimpleQueue()
+    score_queue = SimpleQueue()
     result_queue = SimpleQueue()
 
     n_map_processes = max(num_cores, 1)
@@ -229,10 +237,10 @@ def scoreDuplicates(records, data_model, classifier, num_cores=1, threshold=0) :
     fillQueue(record_pairs_queue, records, n_map_processes)
 
     result = result_queue.get()
-    if isinstance(result, Exception) :
+    if isinstance(result, Exception):
         raise ChildProcessError
 
-    if result :
+    if result:
         scored_pairs_file, dtype, size = result
         scored_pairs = numpy.memmap(scored_pairs_file,
                                     dtype=dtype,
@@ -245,10 +253,11 @@ def scoreDuplicates(records, data_model, classifier, num_cores=1, threshold=0) :
 
     return scored_pairs
 
-def fillQueue(queue, iterable, stop_signals) :
+
+def fillQueue(queue, iterable, stop_signals):
     iterable = iter(iterable)
     chunk_size = 10000
-    upper_bound = 7000000 # this number worked, but is unprincipled 
+    upper_bound = 7000000  # this number worked, but is unprincipled
     multiplier = 1.1
 
     # initial values
@@ -257,42 +266,42 @@ def fillQueue(queue, iterable, stop_signals) :
     t0 = time.clock()
     last_rate = 10000
 
-    while True :
+    while True:
         chunk = tuple(itertools.islice(iterable, int(chunk_size)))
-        if chunk :
+        if chunk:
             queue.put(chunk)
             del chunk
-            
+
             n_records += chunk_size
             i += 1
 
-            if i % 10 :
+            if i % 10:
                 time_delta = max(time.clock() - t0, 0.0001)
 
-                current_rate = n_records/time_delta
+                current_rate = n_records / time_delta
 
                 # chunk_size is always either growing or shrinking, if
                 # the shrinking led to a faster rate, keep
                 # shrinking. Same with growing. If the rate decreased,
                 # reverse directions
-                if current_rate < last_rate :
-                    multiplier = 1/multiplier
+                if current_rate < last_rate:
+                    multiplier = 1 / multiplier
 
                 chunk_size = min(max(chunk_size * multiplier, 1), upper_bound)
 
                 last_rate = current_rate
                 n_records = 0
                 t0 = time.clock()
-                
 
-        else :
+        else:
             # put poison pills in queue to tell scorers that they are
             # done
             [queue.put(None) for _ in range(stop_signals)]
             break
 
-class ScoreGazette(object) :
-    def __init__(self, data_model, classifier, threshold) :
+
+class ScoreGazette(object):
+    def __init__(self, data_model, classifier, threshold):
         self.data_model = data_model
         self.classifier = classifier
         self.threshold = threshold
@@ -302,14 +311,14 @@ class ScoreGazette(object) :
         records = []
 
         for record_pair in block:
-            ((id_1, record_1, _), 
+            ((id_1, record_1, _),
              (id_2, record_2, _)) = record_pair
 
             ids.append((id_1, id_2))
             records.append((record_1, record_2))
 
         distances = self.data_model.distances(records)
-        scores = self.classifier.predict_proba(distances)[:,-1]
+        scores = self.classifier.predict_proba(distances)[:, -1]
 
         mask = scores > self.threshold
         id_type = sniff_id_type(ids)
@@ -327,10 +336,10 @@ class ScoreGazette(object) :
         return scored_pairs
 
 
-def scoreGazette(records, data_model, classifier, num_cores=1, threshold=0) :
-    if num_cores < 2 :
+def scoreGazette(records, data_model, classifier, num_cores=1, threshold=0):
+    if num_cores < 2:
         imap = map
-    else :
+    else:
         from .backport import Pool
         n_map_processes = max(num_cores, 1)
         pool = Pool(processes=n_map_processes)
@@ -346,58 +355,59 @@ def scoreGazette(records, data_model, classifier, num_cores=1, threshold=0) :
         yield scored_pairs
 
 
-def peek(records) :
-    try :
+def peek(records):
+    try:
         record = next(records)
     except TypeError as e:
-        if "not an iterator" not in str(e) :
+        if "not an iterator" not in str(e):
             raise
-        try :
+        try:
             records = iter(records)
             record = next(records)
-        except StopIteration :
+        except StopIteration:
             return None, records
-    except StopIteration :
+    except StopIteration:
         return None, records
-    
-
 
     return record, itertools.chain([record], records)
 
 
-def isIndexed(data, offset) :
+def isIndexed(data, offset):
     return all(i in data for i in range(offset, offset + len(data)))
 
-def index(data, offset=0) :
+
+def index(data, offset=0):
     if isIndexed(data, offset):
         return data
-    else :
-        data = dict(zip(itertools.count(offset), 
+    else:
+        data = dict(zip(itertools.count(offset),
                         viewvalues(data)))
         return data
 
-def iunzip(iterable, internal_length): # pragma: no cover
-    """Iunzip is the same as zip(*iter) but returns iterators, instead of 
+
+def iunzip(iterable, internal_length):  # pragma: no cover
+    """iunzip is the same as zip(*iter) but returns iterators, instead of
     expand the iterator. Mostly used for large sequence"""
 
     _tmp, iterable = itertools.tee(iterable, 2)
     iters = itertools.tee(iterable, internal_length)
     return (map(operator.itemgetter(i), it) for i, it in enumerate(iters))
 
+
 def Enumerator(start=0, initial=()):
-    try : # py 2
+    try:  # py 2
         return collections.defaultdict(itertools.count(start).next, initial)
-    except AttributeError : # py 3
+    except AttributeError:  # py 3
         return collections.defaultdict(itertools.count(start).__next__, initial)
 
 
 def sniff_id_type(ids):
     example = ids[0][0]
     python_type = type(example)
-    if python_type is binary_type or python_type is text_type :
+    if python_type is binary_type or python_type is text_type:
         python_type = (unicode, 256)
     else:
-        int_type(example) # make sure we can cast to int
+        int_type(example)  # make sure we can cast to int
         python_type = int_type
 
     return python_type
