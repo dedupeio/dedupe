ccfe965884d67b94b1ac01e7808a6609ec36ac94
Forest Gregg <fgregg@uchicago.edu>
2018-02-22 23:08:31 -0600

    flake8 backport

--- dedupe/backport.py
+++ dedupe/backport.py
@@ -16,7 +16,7 @@ config_info = str([value for key, value in
                    viewitems(numpy.__config__.__dict__)
                    if key.endswith("_info")]).lower()
 
-if "accelerate" in config_info or "veclib" in config_info :
+if "accelerate" in config_info or "veclib" in config_info:
     warnings.warn("NumPy linked against 'Accelerate.framework'. "
                   "Multiprocessing will be disabled."
                   " http://mail.scipy.org/pipermail/numpy-discussion/2012-August/063589.html")
@@ -25,20 +25,19 @@ elif "mkl_core" in config_info:
     warnings.warn("Numpy linked against MKL. "
                   "Multiprocessing will be disabled. https://github.com/joblib/joblib/issues/138")
     MULTIPROCESSING = False
-elif platform.system() == 'Windows' :
-    logger.warning("Dedupe does not currently support multiprocessing on Windows")
+elif platform.system() == 'Windows':
+    logger.warning(
+        "Dedupe does not currently support multiprocessing on Windows")
     MULTIPROCESSING = False
 
-if MULTIPROCESSING :        
+if MULTIPROCESSING:
     from multiprocessing import Process, Pool, Queue
     if sys.version < '3':
         from multiprocessing.queues import SimpleQueue
-    else :
+    else:
         from multiprocessing import SimpleQueue
-else :
-    if not hasattr(threading.current_thread(), "_children"): 
+else:
+    if not hasattr(threading.current_thread(), "_children"):
         threading.current_thread()._children = weakref.WeakKeyDictionary()
-    from multiprocessing.dummy import Process, Pool, Queue
+    from multiprocessing.dummy import Process, Pool, Queue  # noqa: F401
     SimpleQueue = Queue
-
-
