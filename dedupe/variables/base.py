5f816328a8eb174a18a06c4b629e916832146ff7
Forest Gregg <fgregg@uchicago.edu>
2018-03-09 11:10:20 -0600

    flake variables

--- dedupe/variables/base.py
+++ dedupe/variables/base.py
@@ -1,58 +1,62 @@
 from dedupe import predicates
 
-class Variable(object) :
-    def __len__(self) :
+
+class Variable(object):
+    def __len__(self):
         return 1
 
-    def __repr__(self) :
+    def __repr__(self):
         return self.name
 
-    def __hash__(self) :
+    def __hash__(self):
         return hash(self.name)
 
-    def __eq__(self, other) :
+    def __eq__(self, other):
         return self.name == other.name
 
-    def __init__(self, definition) :
+    def __init__(self, definition):
 
-        if definition.get('has missing', False) :
+        if definition.get('has missing', False):
             self.has_missing = True
-            try :
-                self.predicates += [predicates.ExistsPredicate(definition['field'])]
-            except AttributeError :
+            try:
+                exists_pred = predicates.ExistsPredicate(definition['field'])
+                self.predicates.append(exists_pred)
+            except AttributeError:
                 pass
-        else :
+        else:
             self.has_missing = False
 
-class DerivedType(Variable) :
+
+class DerivedType(Variable):
     type = "Derived"
 
-    def __init__(self, definition) :
-        self.name = "(%s: %s)" % (str(definition['name']), 
+    def __init__(self, definition):
+        self.name = "(%s: %s)" % (str(definition['name']),
                                   str(definition['type']))
         super(DerivedType, self).__init__(definition)
 
 
-class MissingDataType(Variable) :
+class MissingDataType(Variable):
     type = "MissingData"
 
-    def __init__(self, name) :
-        
+    def __init__(self, name):
+
         self.name = "(%s: Not Missing)" % name
 
         self.has_missing = False
 
-class FieldType(Variable) :
+
+class FieldType(Variable):
     _index_thresholds = []
     _index_predicates = []
     _Predicate = predicates.SimplePredicate
 
-    def __init__(self, definition) :
+    def __init__(self, definition):
         self.field = definition['field']
 
-        if 'variable name' in definition :
-            self.name = definition['variable name'] 
-        else :
+        if 'variable name' in definition:
+            self.name = definition['variable name']
+        else:
             self.name = "(%s: %s)" % (self.field, self.type)
 
         self.predicates = [self._Predicate(pred, self.field)
@@ -62,41 +66,40 @@ class FieldType(Variable) :
                                            self._index_thresholds,
                                            self.field)
 
-
         super(FieldType, self).__init__(definition)
 
-class CustomType(FieldType) :
+
+class CustomType(FieldType):
     type = "Custom"
     _predicate_functions = []
 
-    def __init__(self, definition) :
+    def __init__(self, definition):
         super(CustomType, self).__init__(definition)
 
-        try :
+        try:
             self.comparator = definition["comparator"]
-        except KeyError :
+        except KeyError:
             raise KeyError("For 'Custom' field types you must define "
                            "a 'comparator' function in the field "
                            "definition. ")
 
-        if 'variable name' not in definition :
-            self.name = "(%s: %s, %s)" % (self.field, 
-                                          self.type, 
+        if 'variable name' not in definition:
+            self.name = "(%s: %s, %s)" % (self.field,
+                                          self.type,
                                           self.comparator.__name__)
 
 
-def allSubclasses(cls) :
-    for q in cls.__subclasses__() :
+def allSubclasses(cls):
+    for q in cls.__subclasses__():
         yield q.type, q
-        for p in allSubclasses(q) :
+        for p in allSubclasses(q):
             yield p
 
-def indexPredicates(predicates, thresholds, field) :
+
+def indexPredicates(predicates, thresholds, field):
     index_predicates = []
-    for predicate in predicates :
-        for threshold in thresholds :
+    for predicate in predicates:
+        for threshold in thresholds:
             index_predicates.append(predicate(threshold, field))
 
     return index_predicates
-
-    
--- dedupe/variables/exists.py
+++ dedupe/variables/exists.py
@@ -2,34 +2,32 @@ from .base import DerivedType
 from categorical import CategoricalComparator
 from .categorical_type import CategoricalType
 
-class ExistsType(CategoricalType) :
+
+class ExistsType(CategoricalType):
     type = "Exists"
     _predicate_functions = []
 
-    def __init__(self, definition) :
+    def __init__(self, definition):
 
-        super(CategoricalType, self ).__init__(definition)
-        
-        self.cat_comparator = CategoricalComparator([0,1])
-  
+        super(CategoricalType, self).__init__(definition)
+
+        self.cat_comparator = CategoricalComparator([0, 1])
+
         self.higher_vars = []
-        for higher_var in self.cat_comparator.dummy_names :
-            dummy_var = DerivedType({'name' : higher_var,
-                                     'type' : 'Dummy',
-                                     'has missing' : self.has_missing})
+        for higher_var in self.cat_comparator.dummy_names:
+            dummy_var = DerivedType({'name': higher_var,
+                                     'type': 'Dummy',
+                                     'has missing': self.has_missing})
             self.higher_vars.append(dummy_var)
 
-    def comparator(self, field_1, field_2) :
-        if field_1 and field_2 :
+    def comparator(self, field_1, field_2):
+        if field_1 and field_2:
             return self.cat_comparator(1, 1)
-        elif field_1 or field_2 :
+        elif field_1 or field_2:
             return self.cat_comparator(0, 1)
-        else :
+        else:
             return self.cat_comparator(0, 0)
-            
 
     # This flag tells fieldDistances in dedupe.core to pass
     # missing values (None) into the comparator
     comparator.missing = True
-
-
