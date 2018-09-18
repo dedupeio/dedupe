d7f910257f0f2526659e666db552135ae1e97db6
Brian White <brian@dogg.ie>
2018-09-18 13:12:25 +0100

    Add some high level metrics on thresholding and matching

--- dedupe/__init__.py
+++ dedupe/__init__.py
@@ -4,3 +4,8 @@ from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
 from dedupe._init import *  # noqa
+
+
+import os
+module_path = os.path.dirname(os.path.abspath(__file__))
+print(f'*** Custom Conjura dedupe fork loaded: {module_path} ***')
