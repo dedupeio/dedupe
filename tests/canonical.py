27968b0131b04c4bf4f1a6d62bc933e7cda575b3
Forest Gregg <fgregg@uchicago.edu>
2019-05-12 14:26:26 -0400

    revert canonical debug

--- tests/canonical.py
+++ tests/canonical.py
@@ -94,13 +94,11 @@ else:
     deduper = dedupe.Dedupe(fields, num_cores=5)
     deduper.sample(data_d, 10000)
     deduper.markPairs(training_pairs)
+
     deduper.train(index_predicates=False)
     with open(settings_file, 'wb') as f:
         deduper.writeSettings(f)
-
-    import pickle
-    with open('active_learner.pickle', 'wb') as f:
-        pickle.dump(deduper.active_learner, f)
+        
 
 
 alpha = deduper.threshold(data_d, 1)
