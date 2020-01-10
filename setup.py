38536843231d8ced86be13f1591ecdce56b7e000
Forest Gregg <fgregg@uchicago.edu>
2020-01-10 10:52:46 -0500

    predicates path

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email='fgregg@datamade.us',
     description='A python library for accurate and scaleable data deduplication and entity-resolution',
     packages=['dedupe', 'dedupe.variables'],
-    ext_modules= cythonize([Extension('dedupe.cpredicates', ['src/cpredicates.pyx'])]),
+    ext_modules= cythonize([Extension('dedupe.cpredicates', ['dedupe/cpredicates.pyx'])]),
     install_requires=install_requires,
     classifiers=[
         'Development Status :: 4 - Beta',
