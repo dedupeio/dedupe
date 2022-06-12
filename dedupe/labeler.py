56e2685c87b2ad85fd5ce94731a54cd6c9840a0f
Nick Crews <nicholas.b.crews@gmail.com>
2022-06-12 15:47:43 -0600

    Fix mypy

--- dedupe/labeler.py
+++ dedupe/labeler.py
@@ -107,7 +107,7 @@ class RLRLearner(sklearn.linear_model.LogisticRegression, ActiveLearner):
         self.distances = numpy.delete(self.distances, index, axis=0)
 
     def mark(self, pairs: TrainingExamples, y: LabelsLike) -> None:
-        y = numpy.concatenate([self.y, y])
+        y = numpy.concatenate([self.y, y])  # type: ignore[arg-type]
         X = numpy.vstack([self.X, self._distances(pairs)])
         self._fit(X, y)
 
