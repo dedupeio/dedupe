665a559f015a6ab1cb85382be97b05f73f4abe17
Forest Gregg <fgregg@datamade.us>
2023-12-16 07:27:27 -0500

    update pre-commit reqs

--- benchmarks/setup.py
+++ benchmarks/setup.py
@@ -5,4 +5,10 @@ if __name__ == "__main__":
     setup(
         name="benchmarks",
         packages=find_packages(),
+        package_data={
+            # If any package contains *.txt or *.json files, include them:
+            "": ["*.csv"],
+            # And include any files found in the 'mypackage/data' directory:
+            "benchmarks": ["datasets/*"],
+        },
     )
