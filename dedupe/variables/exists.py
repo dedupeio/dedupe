9f8e6f77490f9686cf4e69f1b6a1dd2055e76e05
Forest Gregg <fgregg@users.noreply.github.com>
2024-07-01 09:52:17 -0400

    fix exists variables, closes #1196 (#1197)
    also close #1198

--- dedupe/variables/exists.py
+++ dedupe/variables/exists.py
@@ -5,11 +5,10 @@ from typing import Any
 from categorical import CategoricalComparator
 
 from dedupe._typing import PredicateFunction
-from dedupe.variables.base import DerivedType
-from dedupe.variables.categorical_type import CategoricalType
+from dedupe.variables.base import DerivedType, FieldType
 
 
-class ExistsType(CategoricalType):
+class ExistsType(FieldType):
     type = "Exists"
     _predicate_functions: list[PredicateFunction] = []
 
