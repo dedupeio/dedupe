d12f6b64d435d48ff27288a70fc1290b3511ccab
Arthur Varkentin <Arthur.Varkentin@gmail.com>
2024-07-02 23:18:18 +0200

    Update test_exists.py

--- tests/test_exists.py
+++ tests/test_exists.py
@@ -11,3 +11,10 @@ class TestExists(unittest.TestCase):
         assert numpy.array_equal(var.comparator(None, None), [0, 0])
         assert numpy.array_equal(var.comparator(1, 1), [1, 0])
         assert numpy.array_equal(var.comparator(1, 0), [0, 1])
+
+    def test_len_higher_vars(self):
+        # The len > 1 is neccessary for the correct processing in datamodel.py
+        var = ExistType("foo")
+        assert len(var) > 1
+        assert len(var.higher_vars) > 1
+        assert len(var) == len(var.higher_vars)
